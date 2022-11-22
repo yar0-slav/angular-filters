f658fe0c9b0b51aad281fce796a8792d2b9a7baa
yaro <hornster1@gmail.com>
2022-11-22 09:13:07 +0100

    event and event properties selection from API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { CustomerFilterComponent } from './customer-filter/customer-filter.component';
 import { CustomerFilterFiltersComponent } from './customer-filter-filters/customer-filter-filters.component';
 import { CustomerFilterStepComponent } from './customer-filter-step/customer-filter-step.component';
 
+import { MatCardModule } from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   declarations: [AppComponent, CustomerFilterComponent, CustomerFilterFiltersComponent, CustomerFilterStepComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatSlideToggleModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MatCardModule, MatFormFieldModule, MatSelectModule, FormsModule, BrowserAnimationsModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/customer-filter-step/customer-filter-step.component.ts
+++ src/app/customer-filter-step/customer-filter-step.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs';
 import { FiltersService } from './../services/filters.service';
-
 
 @Component({
   selector: 'app-customer-filter-step',
   templateUrl: './customer-filter-step.component.html',
-  styleUrls: ['./customer-filter-step.component.scss']
+  styleUrls: ['./customer-filter-step.component.scss'],
 })
-export class CustomerFilterStepComponent implements OnInit  {
-
-  customerFilters: any;
-
-  constructor(private filtersService: FiltersService) { }
+export class CustomerFilterStepComponent implements OnInit {
+  events: any;
+  selectedEvent = '';
+  eventProperties: any;
+  constructor(private filtersService: FiltersService) {}
 
   ngOnInit(): void {
     this.fetchData();
   }
 
+  getSelectedEventProperties() {
+    console.log(this.selectedEvent);
+    const { data: { events } } = this.events;
+    const filtered = events.filter(
+      (option: { type: string }) => option.type === this.selectedEvent
+    );
+    console.log(filtered[0].properties);
+    this.eventProperties = filtered[0].properties;
+    return filtered[0].properties;
+  }
+
+  currentlySelectedEvent(e: string) {
+    console.log(this.selectedEvent);
+    return e;
+  }
+
   fetchData(): void {
-    this.filtersService.getFiltersData().subscribe((res) => {
-      this.customerFilters = res;
-    })
+    this.filtersService
+      .getFiltersData()
+      .pipe(take(1))
+      .subscribe((data) => (this.events = data));
   }
 }
