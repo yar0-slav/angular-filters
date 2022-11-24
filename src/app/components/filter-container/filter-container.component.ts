23942ddb6266ebbd96ca98e56ca4b35c3e6ea413
yaro <hornster1@gmail.com>
2022-11-24 23:43:37 +0100

    removing form from constructor

--- src/app/components/filter-container/filter-container.component.ts
+++ src/app/components/filter-container/filter-container.component.ts
@@ -1,39 +1,54 @@
 import { Component } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
+import { take } from 'rxjs';
+import { FiltersService } from 'src/app/services/filters.service';
+
+
 @Component({
   selector: 'app-filter-container',
   templateUrl: './filter-container.component.html',
   styleUrls: ['./filter-container.component.scss'],
 })
 export class FilterContainerComponent {
-  filterForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.filterForm = this.fb.group({
-      filters: this.fb.array([
-        this.fb.group({
-          eventType: [''],
-          eventProperties: this.fb.array([
-            this.fb.group({
-              property: [''],
-              type: [''],
-              operator: this.fb.group({
-                name: [''],
-                values: this.fb.array([
-                  ['']
-                ])
-              })
+
+  constructor(private fb: FormBuilder) { }
+
+  filterForm: FormGroup = this.fb.group({
+    filters: this.fb.array([
+      this.fb.group({
+        eventType: [''],
+        eventProperties: this.fb.array([
+          this.fb.group({
+            property: [''],
+            type: [''],
+            operator: this.fb.group({
+              name: [''],
+              values: this.fb.array([
+                ['']
+              ])
             })
-         ])
-        }),
-      ]),
-    });
-  }
+          })
+        ])
+      }),
+    ]),
+  });
+
 
 
   onClickSubmit(): void {
     console.log(this.filterForm.getRawValue());
   }
+
+  // fetchData(): void {
+  //   this.filtersService
+  //     .getFiltersData()
+  //     .pipe(take(1))
+  //     .subscribe((data) => {
+  //       console.log('api data', data);
+  //       this.events = data;
+  //     });
+  // }
 
 }
